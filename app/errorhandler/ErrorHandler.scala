36cf432074ebc3f008450b2c33942763fec3a748
fabian <f.markert87@googlemail.com>
2016-05-09 22:18:14 +0200

    add error messages

--- app/errorhandler/ErrorHandler.scala
+++ app/errorhandler/ErrorHandler.scala
@@ -5,10 +5,12 @@ import javax.inject._
 import controllers.WebJarAssets
 import play.api._
 import play.api.http.DefaultHttpErrorHandler
+import play.api.http.Status._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.Results._
 import play.api.mvc._
 import play.api.routing.Router
+import views.html.errorpages._
 
 import scala.concurrent._
 
@@ -27,6 +29,18 @@ class ErrorHandler @Inject()(implicit val messagesApi: MessagesApi, env: Environ
     implicit val req = request.asInstanceOf[Request[_]]
 
     implicit val webJarAssets: WebJarAssets = new WebJarAssets(this, config, env)
-    Future.successful(NotFound(views.html.errorpages.pageNotFound()))
+    Future.successful(NotFound(pageNotFound()))
+  }
+
+  override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+    implicit val req = request.asInstanceOf[Request[_]]
+
+    implicit val webJarAssets: WebJarAssets = new WebJarAssets(this, config, env)
+    statusCode match {
+      case NOT_FOUND => onNotFound(request, message)
+      case FORBIDDEN => Future.successful(Forbidden(errorPage(statusCode.toString, message)))
+      case BAD_REQUEST => Future.successful(BadRequest(errorPage(statusCode.toString, message)))
+      case _ => Future.successful(Ok(errorPage(statusCode.toString, message)))
+    }
   }
 }
